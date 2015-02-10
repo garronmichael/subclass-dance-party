1340d8ca5583a05e3f8f5018b41a9fc795472bc2
garronmichael <garron.michael@gmail.com>
2015-02-09 16:07:35 -0800

    Create BlueDancer Add .gitignore file for .DS_Store

---
+++ src/blueDancer.js
@@ -0,0 +1,23 @@
+var BlueDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+BlueDancer.prototype = Object.create(Dancer.prototype);
+BlueDancer.prototype.constructor = BlueDancer;
+
+BlueDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.css({
+  border: '10px solid blue'
+  });
+
+};
+BlueDancer.prototype.oldStep = Dancer.prototype.step;
